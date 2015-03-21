4001927cd7c7341a1d3637d6e99bc18b7e79d66a
Simone Todaro <todaro@adacto.it>
2015-03-21 02:09:51 +0100

    Use Psr\Http\Message\ResponseInterface

--- src/Download.php
+++ src/Download.php
@@ -1,33 +1,34 @@
 <?php
 namespace SimoTod\SlimDownload;
 
-class DownloadView implements \Pimple\ServiceProviderInterface
-{
-	protected response;
-	
-	protected $app;
-	
-    public function register(\Slim\App $container)
+use Psr\Http\Message\ResponseInterface;
+
+class Download implements \Pimple\ServiceProviderInterface
+{	
+    public function register(\Pimple\Container $container)
     {
-        $container['view'] = $this;
-		$this->app = $container;
+        $container['download'] = $this;
     }
     
-    public function render($path, $data = []) 
+    public function create(ResponseInterface $response, $path, $data = []) 
     {        
         $contentType = $data["CONTENT_TYPE"] ? $data["CONTENT_TYPE"] : 'application/octet-stream';
         $filename = $data["FILENAME"] ? $data["FILENAME"] : basename($path);
 		
-		$newResponse = $this->app->response->withStatus(200)
+		$newResponse = $response->withStatus(200)
 			->withHeader('Content-Type', $contentType)
 			->withAddedHeader('Content-Transfer-Encoding', 'Binary');
 			->withAddedHeader('Content-disposition', 'attachment; filename="'.$filename.'"')
 			->withAddedHeader('Content-Length', filesize($path))
 			->withAddedHeader('Expires', '0')
 			->withAddedHeader('Cache-Control', 'must-revalidate, post-check=0, pre-check=0')
-			->withAddedHeader('Pragma', 'public')
-			->withBody(new Body(fopen($path, 'r')));
+			->withAddedHeader('Pragma', 'public');
+		
+        ob_start();
+        readfile($path);
+        $content = ob_get_clean();
+		$newResponse = $newResponse->withBody($content);
 
-        $this->app->stop($newResponse);
+        return $newResponse;
     }
 }
